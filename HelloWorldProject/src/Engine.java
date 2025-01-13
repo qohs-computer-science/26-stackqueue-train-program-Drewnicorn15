9ed0ca1f2bc01fa1e9ef424d80e4361699a6989b
Drewnicorn15 <67112695+Drewnicorn15@users.noreply.github.com>
2025-01-13 15:44:25 +0000

    sumbitsiom

---
+++ HelloWorldProject/src/Engine.java
@@ -0,0 +1,19 @@
+public class Engine{
+    private String name, destination;
+    //constuctors
+    public Engine(String n,String d)
+    {
+        name = n;
+        destination = d;
+    }
+    //getters
+    public String getName()
+    {
+        return name;
+    }
+    public String getDestination()
+    {
+        return destination;
+    }
+
+}
